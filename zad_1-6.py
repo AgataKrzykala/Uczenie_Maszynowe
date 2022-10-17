74220c6b0b44e79445fc85b8d5f29a0530c7dc1b
AgataKrzykala <agata.krzykala@edu.uekat.pl>
2022-10-17 17:35:15 +0200

    pycodestyle done

--- zad_1-6.py
+++ zad_1-6.py
@@ -4,56 +4,74 @@ Created on Mon Oct 10 14:38:09 2022
 
 @author: agata
 """
-#1
-def funkcja(name: str,surname: str) -> str:
+# 1
+
+
+def funkcja(name: str, surname: str) -> str:
     string = f"Czesc {name} {surname}!"
     return string
+
 
 text = funkcja("agata", "krzykała")
 print(text)
 
-#2
+# 2
+
+
 def funkcja2(a: int, b: int):
-    return a*b
+    return a * b
 
-print(funkcja2(1,4))
 
-#3
+print(funkcja2(1, 4))
+
+# 3
+
+
 def funkcja3(a: int) -> bool:
-    if a%2==0:
+    if a % 2 == 0:
         return True
     else:
         return False
 
+
 wynik = funkcja3(5)
-if wynik == True:
+if wynik is True:
     print("Liczba parzysta")
 else:
     print("Liczba nieparzysta")
-    
-#4
-def funkcja4(a:int,b:int,c:int) -> bool:
-    if (a+b >= c):
+
+# 4
+
+
+def funkcja4(a: int, b: int, c: int) -> bool:
+    if (a + b >= c):
         return True
     else:
         return False
-    
-print(funkcja4(1,2,5))
-    
-#5
-def funkcja5(lista:list,a:int) -> bool:
+
+
+print(funkcja4(1, 2, 5))
+
+# 5
+
+
+def funkcja5(lista: list, a: int) -> bool:
     if a in lista:
         return True
     else:
         return False
-    
-print(funkcja5([1,2,3],5))
 
-#6
-def funkcja6(a:list,b:list) -> list:
-    lista = a+b
+
+print(funkcja5([1, 2, 3], 5))
+
+# 6
+
+
+def funkcja6(a: list, b: list) -> list:
+    lista = a + b
     lista = [x**3 for x in lista]
     lista = list(dict.fromkeys(lista))
     return lista
 
-print(funkcja6([1,2,2,3,3,4,5],[1,2,6,7,7,8]))
+
+print(funkcja6([1, 2, 2, 3, 3, 4, 5], [1, 2, 6, 7, 7, 8]))

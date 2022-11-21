79cc2f99fb7799ade598f2b511339dd36e99b60f
AgataKrzykala <agata.krzykala@edu.uekat.pl>
2022-11-21 14:47:50 +0100

    dodatkowy range na red

--- skrypt.py
+++ skrypt.py
@@ -12,7 +12,7 @@ import numpy as np
 cap = cv2.VideoCapture('rundaa.MP4')
 
 fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-video_output = cv2.VideoWriter('output.mp4', fourcc, 50.0, (1920, 1080))
+video_output = cv2.VideoWriter('output2.mp4', fourcc, 50.0, (1920, 1080))
 
 while (1):
 
@@ -26,18 +26,23 @@ while (1):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
-    lower_blue = np.array([110, 50, 50])
-    upper_blue = np.array([130, 255, 255])
+    lower_blue = np.array([94, 80, 2])
+    upper_blue = np.array([126, 255, 255])
 
     # Threshold the HSV image to get only blue colors
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
 
-    lower_red = np.array([30, 150, 50])
-    upper_red = np.array([235, 255, 180])
+    lower_red1 = np.array([170,150,50])
+    upper_red1 = np.array([180,255,255])
+    
+    mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    
+    lower_red = np.array([0, 150, 50])
+    upper_red = np.array([5, 255, 255])
 
     mask_red = cv2.inRange(hsv, lower_red, upper_red)
 
-    mask = mask_blue + mask_red
+    mask = mask_blue + mask_red + mask_red1
 
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame, frame, mask=mask)
